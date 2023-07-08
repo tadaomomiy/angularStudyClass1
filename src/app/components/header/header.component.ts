fb9342c6e417b49a9c32d01250cb77fdae9e8f8e
Matheus Momiy <matheus.tadao@gmail.com>
2023-07-09 08:49:05 +0900

    add path home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(){}
+  @Input() nome:string = "";
+  ngOnInit():void{
+
+  }
 }
